2f639f411554b5cfdd5cbf62a54ef90a4680f70a
Federico Nelli <federiconellisfe@gmail.com>
2024-07-17 22:08:27 -0300

    Minor fixes cardLg and cardMd

--- client/src/components/cards/cardLg.jsx
+++ client/src/components/cards/cardLg.jsx
@@ -4,17 +4,15 @@ import { Button } from "../button/button"
 function CardLg() {
     return (
         <>
-            <article class="p-10 min-h-116 max-w-3xl w-full bg-sipe-white bg-opacity-10 rounded-xl text-sipe-white xl:col-span-2 transform duration-500 hover:-translate-y-1 cursor-pointer">
+            <article className="p-12 max-h-full w-full bg-sipe-white bg-opacity-10 rounded-xl text-sipe-white xl:col-span-2 transform duration-500 hover:-translate-y-1 cursor-pointer">
                 <div className="min-h-62 flex justify-center">
                     <img src="src\assets\images\icons\materialesResumen.png" alt="" />
                 </div>
-                <h1 class="flex justify-center mt-5 text-5xl font-light text-gray-100 leading-snug  min-h-33">Últimos movimientos
-                </h1>
-                <div class="flex justify-center text-justify mt-4">
-                    <p class="font-light">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore in, hic ipsa, magnam exercitationem provident, necessitatibus delectus dignissimos ipsum veritatis mollitia. Sit, blanditiis qui. A rem velit voluptatum praesentium cumque!
-                    Totam numquam molestias minima doloremque suscipit sed cum nihil non in quam eius odio nobis voluptatum modi voluptates at praesentium assumenda eos nam earum, voluptate accusamus! Sunt veritatis eveniet perspiciatis.</p>
+                <h1 className="flex justify-center mt-5 text-5xl font-light text-gray-100 leading-snug min-h-33">Últimos movimientos</h1>
+                <div className="flex justify-center text-justify mt-4 w-full">
+                    <p className="font-light">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore in, hic ipsa, magnam exercitationem provident, necessitatibus delectus dignissimos ipsum veritatis mollitia.</p>
                 </div>
-                <div class="mt-16 flex justify-center ">
+                <div className="mt-8 flex justify-center ">
                     <Link to="/materiales">
                         <Button variant="sipebutton" size="sipebutton" type="submit">
                             Ir a Materiales
--- client/src/pages/dashboard/dashboard.jsx
+++ client/src/pages/dashboard/dashboard.jsx
@@ -2,19 +2,21 @@ import MainDashboard from "@/components/mainDashboard/mainDashboard"
 import Navbar from "@/components/navbar/navbar"
 import Sidenav from "@/components/sidenav/sidenav"
 
-
 function Dashboard() {
     return (
-        <section className="min-h-screen flex items-middle bg-sipe-gradient">
+        <section className="min-h-screen flex bg-sipe-gradient">
             <div>
                 <Sidenav />
             </div>
-            <div className="w-full">
+            <div className="w-full flex flex-col">
                 <Navbar />
                 <hr />
-                <div className="bg-sipe-gradient p-5 md:p-0 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-10">
-                    <MainDashboard />
-                </div>
+                
+                    <div className="p-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-5">
+                        <MainDashboard />
+                    </div>
+                
+                
             </div>
         </section>
     )
